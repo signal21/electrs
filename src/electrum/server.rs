eaf8329a278a67a0e32ca9e835fc2008d8f2b55a
rem1-dev <150667999+rem1-dev@users.noreply.github.com>
2024-02-20 16:35:28 +0100

    Removing unnecessary json! call

--- src/electrum/server.rs
+++ src/electrum/server.rs
@@ -509,7 +509,7 @@ impl Connection {
                 "port": self.addr.port(),
             }),
         );
-        println!("ELECTRUM-RPC-LOGGER: {}", json!(log));
+        println!("ELECTRUM-RPC-LOGGER: {}", log);
     }
 
     fn send_values(&mut self, values: &[Value]) -> Result<()> {
